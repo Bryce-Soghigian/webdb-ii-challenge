f5be5a638a979393360082d3b15c301cde449908
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-08-13 16:37:06 -0500

    added exports.down to my migrations

--- db/migrations/20190813153818_cars.js
+++ db/migrations/20190813153818_cars.js
@@ -1,22 +1,41 @@
 
-var pg = require('knex')({client: 'pg'});
+
 exports.up = function(knex, Promise) {
+
+
+    return knex.schema
+    .createTable("carz",tbl => {
+        tbl.increments()
+        //criticals
+        //notNullable === included in yup
+        tbl.text("VIN",100).notNullable().unique();
+        tbl.text("make").notNullable();
+        tbl.text("model").notNullable();
+        tbl.integer("mileage").notNullable();
+        tbl.text("transtype");
+        tbl.text("status");
+    })
+
     // VIN idnum,
     // make chevy,
     // model 6969,
     // mileage 45000,
-    // status salvage,
-   knex.schema.createtable("cars");
-   knex('cars').insert({VIN:'idnum'}).returning('*').toString();
-   knex('cars').insert({Make: 'Camaro' }).returning('*').toString();
-   knex('cars').insert({model:"SS"}).returning('*').toString();
-   knex('cars').insert({mileage:"45000"}).returning('*').toString();
-   knex('cars').insert({status:"salvage"}).returning('*').toString();
+//     // status salvage,
+//    knex.schema.createtable("cars");
+//    knex('cars').insert({VIN:'idnum'}).returning('*').toString();
+//    knex('cars').insert({Make: 'Camaro' }).returning('*').toString();
+//    knex('cars').insert({model:"SS"}).returning('*').toString();
+//    knex('cars').insert({mileage:"45000"}).returning('*').toString();
+//    knex('cars').insert({status:"salvage"}).returning('*').toString();
 
 
 };
 
-exports.down = function(knex) {
+exports.down = function(knex,Promise) {
+
+    return knex.schema.dropTable('carz');
+
+
   
 };
 // The client for this API is a car dealer who has provided the following specs:
